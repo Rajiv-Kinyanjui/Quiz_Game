9b73377b456223c213ac80beb5537992b0953b57
Rajiv-Kinyanjui <peterkinyanjui226@gmail.com>
2022-04-07 22:05:53 +0300

    Define a function that takes in x,y and z then print a welcoming message that prompts the user to give a response on whether the want to play the game

---
+++ game.py
@@ -0,0 +1,8 @@
+def give_options(x,y,z):
+    print("a):",x)
+    print("b):",y)
+    print("c):",z)
+
+print("Hello! Welcome to my quiz \nEvery answer is worth 10 points")
+ans = raw_input("\nAre you ready to play?(Yes/No)")
+
